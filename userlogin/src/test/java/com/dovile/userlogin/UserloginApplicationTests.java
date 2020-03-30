96d9fbdb6a07538eac1ee99701f0881e5b94afb6
Doirena <56863735+Doirena@users.noreply.github.com>
2020-03-30 19:51:05 +0300

    Initial commit

--- userlogin/src/test/java/com/dovile/userlogin/UserloginApplicationTests.java
+++ userlogin/src/test/java/com/dovile/userlogin/UserloginApplicationTests.java
@@ -1,13 +1,34 @@
 package com.dovile.userlogin;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dovile.userlogin.entities.User;
+import com.dovile.userlogin.repos.UserRepository;
 
 @SpringBootTest
 class UserloginApplicationTests {
+//	@Test
+//	void contextLoads() {
+//	}
+	
+	@Autowired
+	private UserRepository userRepository;
 
+	
 	@Test
-	void contextLoads() {
+	public void testCreateUser() {
+
+		User user = new User();
+		user.setEmail("email2@gmail");
+		user.setPassword("test");
+		User saveUser = userRepository.save(user);
+		assertThat(saveUser.getEmail()).isNotNull();
+		
 	}
 
 }
